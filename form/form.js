700f668f666bf1af57b26be08e6a1ae4e60dd014
sumit <sumitrajput88956@gmail.com>
2025-01-31 22:58:53 +0530

    update

--- form/form.js
+++ form/form.js
@@ -12,16 +12,49 @@ let sub=()=>{
    let errpass=document.querySelector("#errorpass")
    let errcpass=document.querySelector("#errorcpass")
 
+   
    if(inpname==""){
     errname.innerHTML="Please fill the Name field"
     errname.style.color="red"
     return false;
    }
 
+
+//    number
+
+else if( isNaN(inpnum)){
+    errnum.innerHTML="please enter fill the number only"
+    return false;
+}
+
+else if (inpnum.length!==10){
+    errnum.innerHTML="please enter 10 digital only"
+    return false 
+}
+
+else if( !(inpemail.includes("@") && inpemail.includes(".com")))
+    {
+    erremail.innerHTML="please enter valid email"
+    return false;
+}
+
+else if (!(inppass.match([/1234567890/])
+
+    && inppass.match([/!@#$%^&*()/])
+    && inppass.match([/a-z/])
+    && inppass.match([/A-Z/])
+
+)){
+    errpass.innerHTML="";
+}
+
+
+
    if(inppass==inpcpass)
    {
     return true;
    }
+
    else{
     errcpass.innerHTML="password not match. plz enter the correct pass"
     errcpass.style.color="red"
