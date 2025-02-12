991c3e80276e99a536c23afce67a47aa5d2d91ee
sumit <sumitrajput88956@gmail.com>
2025-02-12 21:42:07 +0530

    Bom

---
+++ Bom/script.js
@@ -0,0 +1,46 @@
+// Checking dynamically ever or odd
+
+function s(){
+    let abc=prompt("Enter number to check")
+    if(abc%2==0)
+    {
+        alert("Ever Number")
+    
+    }
+    else {
+        alert("odd")
+    }
+    }
+    s();
+    
+    function age(){
+        let a=prompt("Enter your age")
+        if(a>=18)
+        {
+            alert("You can vote")
+        
+        }
+        else {
+            alert("You cannot vote")
+        }
+        }
+        age();
+    
+    
+        function age1(){
+            let a= prompt("Enter your age");
+            (a>=18)?alert("you can vote"):alert("you cant vote");
+          
+        
+        }
+        age1();
+    
+        function age2(){
+            for(let i=1;i<=10;i++)
+            {
+                console.log(i);
+            }
+          
+        
+        }
+        age2();
