665bdf747713a3726616ce47ef11454c72040be8
sumit <sumitrajput88956@gmail.com>
2025-02-12 21:33:59 +0530

    Arrow

---
+++ Arrow function/script.js
@@ -0,0 +1,12 @@
+let a=(n,n1)=>{
+    console.log(`my name is ${n} and age my age is ${n1}`)
+    let b=document.querySelector("#s")
+b.innerHTML=`My name is ${n} and age is ${n1}`
+}
+
+
+let s=()=>{
+    let input= document.querySelector("#f").value;
+    alert(value)
+
+}
