93adc26aeee586074899c71b8f22f30aeafefbf4
sumit <sumitrajput88956@gmail.com>
2025-02-20 02:05:18 +0530

    Color Change

---
+++ js Q/Color_change.js
@@ -0,0 +1,11 @@
+function s(){
+    let box=document.querySelector("#box")
+    box.style.backgroundColor="green"
+
+}
+
+function r(){
+    let box1=document.querySelector("#box")
+    box1.style.backgroundColor="orange"
+
+}
