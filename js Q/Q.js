c27c17eec597377d55cf259843a98470482b1330
sumit <sumitrajput88956@gmail.com>
2025-02-20 01:45:11 +0530

    Positive Negative or Zero

---
+++ js Q/Q.js
@@ -0,0 +1,17 @@
+let b=parseInt(prompt("Enter any no"))
+
+let a=()=>{
+   if(b>0)
+   {
+   alert("positive")
+   }
+   else if (b<0)
+    {
+    alert("negative")
+    }
+   else{
+    alert("zero")
+   }
+
+}
+a();
