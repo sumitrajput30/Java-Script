35d253cd20bbfb24aeabe7ebec52556af0b0f3b8
sumit <sumitrajput88956@gmail.com>
2025-02-20 01:54:25 +0530

    Random No

---
+++ js Q/Random_no.js
@@ -0,0 +1,17 @@
+let s=()=>{
+    let a= document.querySelector("#a")
+    a.innerHTML=Math.floor(Math.random()*10)
+  }
+  s()
+  
+  let s1=()=>{
+      let a= document.querySelector("#a1")
+       a.innerHTML=Math.floor(Math.random()*10)
+    }
+    s()
+  
+    let s2=()=>{
+      let a= document.querySelector("#a2")
+      a.innerHTML=Math.floor(Math.random()*10)
+    }
+    s()
