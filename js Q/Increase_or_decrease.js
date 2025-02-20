4d59b06b82b18d3d2243f1a6226620b0cd9c78b8
sumit <sumitrajput88956@gmail.com>
2025-02-21 00:25:59 +0530

    Increase and Decrease

---
+++ js Q/Increase_or_decrease.js
@@ -0,0 +1,16 @@
+let count=0;
+function i(){
+  
+    count++;
+    let display=document.querySelector("#show")
+    display.innerHTML=count
+
+}
+
+function d(){
+  
+    count--;
+    let display=document.querySelector("#show")
+    display.innerHTML=count
+
+}
