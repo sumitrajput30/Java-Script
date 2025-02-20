08f4ebea54165d71751ec6da229b780624838a01
sumit <sumitrajput88956@gmail.com>
2025-02-21 00:43:38 +0530

    image change

---
+++ js Q/img.js
@@ -0,0 +1,24 @@
+function a(){
+    let d=document.querySelector("#s");
+     d.style.backgroundImage="url('download (1).jpg')"
+ }
+ 
+ function b(){
+     let e=document.querySelector("#s");
+     e.style.backgroundImage="url('download (2).jpg')"
+ }
+ 
+ function c(){
+     let f=document.querySelector("#s");
+     f.style.backgroundImage="url('download.jpg')" 
+ }
+ 
+ 
+ function resetimg(){
+     let h= document.querySelector("#s");
+     h.style.backgroundImage="none"
+    
+ }
+ 
+ 
+ 
