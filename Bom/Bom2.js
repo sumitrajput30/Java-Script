dca83411aee3997570a02b87fbc89aa0d70cf69b
sumit <sumitrajput88956@gmail.com>
2025-02-21 00:33:52 +0530

    Bom

---
+++ Bom/Bom2.js
@@ -0,0 +1,41 @@
+function a(){
+    let ans=parseInt(prompt("enter number"))
+
+    let dis=confirm("Do you have any coupan")
+
+    if(dis)
+    {
+        alert(ans*0.9)
+    }
+    else {
+        alert(ans)
+    }
+
+
+
+    q9
+
+     let ans1;
+     do{
+        ans1=parseInt(prompt("Enter number"))
+     }
+        while(ans1<=10)
+     
+}
+ a();
+
+
+function a(){
+    alert("a")
+}
+
+
+
+function b(){
+    alert("b")
+}
+
+
+function c(){
+    alert("c")
+}
