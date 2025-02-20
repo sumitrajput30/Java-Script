66517ba76a4dba73a8480748a9e5eebbd25df6b2
sumit <sumitrajput88956@gmail.com>
2025-02-21 00:38:26 +0530

    Background color or text color change

---
+++ Bom/text.js
@@ -0,0 +1,17 @@
+let a=()=>{
+    let color = document.querySelector("#s"); 
+    // let b=document.querySelector("#d")
+
+  
+   
+        if (document.body.style.backgroundColor=="white") {
+         color.style.color="black"
+        }
+      
+       else{
+        document.body.style.backgroundColor="black"
+         color.style.color="white"
+       }
+
+}
+
